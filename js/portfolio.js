871ead78eac3947643cbeb7c0137aa859e645d23
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2022-05-20 17:23:02 -0300

    feat: improved error handling for missing exchange rates

--- js/portfolio.js
+++ js/portfolio.js
@@ -7,21 +7,33 @@ class Portifolio {
         this.moneys.concat(Array.prototype.slice.call(arguments));
     }
     evaluate(currency){
+        let failures = [];
         let total = this.moneys.reduce((sum, money) => {
-            return sum + this.convert(money, currency);
+            let convertedAmount = this.convert(money, currency);
+            if(convertedAmount == undefined){
+                failures.push(money.currency+"->"+currency);
+                return sum;
+            }
+            return sum + convertedAmount;
         }, 0);
-        return new Money(total, currency);
+        if(failures.length == 0){
+            return new Money(total, currency);
+        }
+        throw new Error("Missing exchange rate(s):["+failures.join()+"]");
     }
     convert(money, currency){
         let exchangeRates = new Map();
         exchangeRates.set('EUR->USD', 1.2);
         exchangeRates.set('USD->KRW', 1100);
-        
-        let key = money.currency + '->' + currency;
         if(money.currency === currency){
             return money.amount;
         }
-        return money.amount * exchangeRates.get(key);
+        let key = money.currency + '->' + currency;
+        let rate = exchangeRates.get(key);
+        if (rate == undefined){
+            return undefined;
+        }
+        return money.amount * rate;
     }
 }
 
